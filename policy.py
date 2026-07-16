f59b8d211b5aff2afb6834b3abd32dd4f0f18008
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-07-16 23:30:42 +0530

    DQN Agent

--- policy.py
+++ policy.py
@@ -48,4 +48,38 @@ class ReplayBuffer:
 
 
 class DQNAgent:
-    
+    def __init__(self,
+                 state_dim: int,
+                 n_actions: int,
+                 device: torch.device,
+                 lr: float=1e-3,
+                 gamma: float = 0.99,
+                 buffer_capacity: int = 50_000,
+                 batch_size: int = 64,
+                 eps_start: float = 1.0,
+                 eps_end: float = 0.02,
+                 eps_decay: float = 0.995,
+                 target_update_freq: int = 10,
+    ):
+        
+        self.device = device
+        self.n_actions = n_actions
+        self.gamma = gamma
+        self.batch_size = batch_size
+ 
+        self.eps = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+ 
+        self.target_update_freq = target_update_freq
+        self.update_count = 0
+ 
+        self.policy_net = QNetwork(state_dim, n_actions).to(device)
+        self.target_net = QNetwork(state_dim, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+ 
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+        self.loss_fn = nn.SmoothL1Loss()  # Huber loss
+ 
+        self.memory = ReplayBuffer(buffer_capacity)
